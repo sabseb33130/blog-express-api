d42ee9af0b2814553ef15bd34e0a314769417866
Titou <menardjc.33@gmail.com>
2023-01-16 13:03:12 +0100

    feat: fichier client,auth

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// imports
+const express = require("express");
+require("dotenv").config(); // permet de cacher les donnée dans un autrs fichier .env
+const bodyParser = require("body-parser");
+const { Client } = require("pg");
+const fs = require("fs");
+
+// declarations
+const app = express();
+const port = 8000;
+const client = new Client({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+client.connect();
+
+//activation bodyparse pour JSON
+app.use(bodyParser.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/user", (req, res) => {
+  res.send("Hello User page!");
+});
+
+// ecoute le port 8000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
