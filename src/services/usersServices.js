14242f5f9975bf867f7d492f91508ce7c982f0c4
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2023-01-17 13:59:12 +0100

    fix: delete et put des articles

--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -5,8 +5,7 @@ class UsersServices {
     const data = await Client.query("SELECT * FROM users WHERE name=$1", [
       name,
     ]);
-    console.log(data);
-    console.log(data.rowCount);
+
     if (data.rowCount == 1) {
       return data.rows[0];
     }
@@ -19,7 +18,7 @@ class UsersServices {
       "INSERT INTO users (name,password) VALUES ($1,$2) RETURNING *",
       [name, hash]
     );
-    console.log(data.rowCount);
+
     if (data.rowCount) {
       return data.rows[0];
     }
